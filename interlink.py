0cde650a5fd598b7fe83d8a88bb6810c7083e44e
Kostis Karantias <kkarantias@gmail.com>
2018-11-26 18:34:25 +0200

    switch to "dumber" impl to aid refactoring

--- interlink.py
+++ interlink.py
@@ -1,28 +1,20 @@
 from merkle import mtr
 
 class Interlink:
-    def __init__(self):
-        self.levels = {}
+    def __init__(self, blocks=None):
+        self.blocks = [] if blocks is None else blocks
 
     def update(self, block_id, level):
-        deprecated_levels = [existing_level for existing_level in
-                sorted(self.levels.keys()) if existing_level <= level]
-        for deprecated_level in deprecated_levels:
-            del self.levels[deprecated_level]
-        self.levels[level] = block_id
-
-    def __getitem__(self, req_level):
-        for level in sorted(self.levels.keys()):
-            if req_level <= level:
-                return self.levels[level]
+        blocks = self.blocks.copy()
+        for i in range(0, level+1):
+            if i < len(blocks):
+                blocks[i] = block_id
+            else:
+                blocks.append(block_id)
+        return Interlink(blocks=blocks)
 
     def as_array(self):
-        array = []
-        prev_lvl = -1
-        for lvl in sorted(self.levels.keys()):
-            array += [self.levels[lvl]] * (lvl - prev_lvl)
-            prev_lvl = lvl
-        return array
+        return self.blocks
 
     def hash(self):
         return mtr(self.as_array())
--- test_interlink.py
+++ test_interlink.py
@@ -2,14 +2,14 @@ from interlink import Interlink
 
 def test_interlink():
     interlink = Interlink()
-    interlink.update('z', 5)
-    interlink.update('y', 4)
-    interlink.update('x', 2)
+    interlink = interlink.update('z', 5)
+    interlink = interlink.update('y', 4)
+    interlink = interlink.update('x', 2)
     assert interlink.as_array() == ['x', 'x', 'x', 'y', 'y', 'z']
 
 def test_interlink_cascading():
     interlink = Interlink()
-    interlink.update('x', 2)
-    interlink.update('y', 4)
-    interlink.update('z', 5)
+    interlink = interlink.update('x', 2)
+    interlink = interlink.update('y', 4)
+    interlink = interlink.update('z', 5)
     assert interlink.as_array() == ['z'] * 6
