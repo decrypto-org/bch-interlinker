9cbfe1a44547562af6ab85c218d51177f11a09fc
Kostis Karantias <kkarantias@gmail.com>
2018-11-28 14:24:01 +0200

    fix interlink tests

--- test_interlink.py
+++ test_interlink.py
@@ -1,15 +1,15 @@
 from interlink import Interlink
 
 def test_interlink():
-    interlink = Interlink()
-    interlink = interlink.update('z', 5)
-    interlink = interlink.update('y', 4)
-    interlink = interlink.update('x', 2)
-    assert interlink.as_array() == ['x', 'x', 'x', 'y', 'y', 'z']
+    interlink = Interlink(genesis=b'\x01')
+    interlink = interlink.update('0c', 5)
+    interlink = interlink.update('0b', 4)
+    interlink = interlink.update('0a', 2)
+    assert interlink.as_array() == [b'\x0a', b'\x0a', b'\x0a', b'\x0b', b'\x0b', b'\x0c', b'\x01']
 
 def test_interlink_cascading():
-    interlink = Interlink()
-    interlink = interlink.update('x', 2)
-    interlink = interlink.update('y', 4)
-    interlink = interlink.update('z', 5)
-    assert interlink.as_array() == ['z'] * 6
+    interlink = Interlink(genesis=b'\x02')
+    interlink = interlink.update('0a', 2)
+    interlink = interlink.update('0b', 4)
+    interlink = interlink.update('0c', 5)
+    assert interlink.as_array() == [b'\x0c'] * 6 + [b'\x02']
